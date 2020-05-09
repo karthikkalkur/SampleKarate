757912ab680f9fe399b50b934a83160722c7d2e0
Karthik Kalkur <kkarthika@athenahealth.com>
2020-05-09 17:26:24 +0530

    Added new files

---
+++ src/main/java/com/karthik/SampleKarate/SampleKarateApplication.java
@@ -0,0 +1,13 @@
+package com.karthik.SampleKarate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleKarateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleKarateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthik/SampleKarate/SampleKarateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthik.SampleKarate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleKarateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
